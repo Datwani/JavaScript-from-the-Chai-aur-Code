53638dff4e1d700d1f102892c129c248b1dbe5c5
Yash_Datwani <126910357+Datwani@users.noreply.github.com>
2025-07-19 13:35:37 +0530

    Add files via upload

---
+++ JavaScript/01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountID =144553
+let accountEmail ="Hello@google.com"
+var accountPassword="12345"
+accountCity ="Jaipur"
+let accountState;
+// accountID=2 // not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="212121221"
+accountCity="delhi"
+
+
+
+console.log(accountID);
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
